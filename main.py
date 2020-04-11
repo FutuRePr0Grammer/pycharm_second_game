f7efa516b026454de7e7198bd675f1385088f66b
David Rochon <drochon59@yahoo.com>
2020-04-11 15:37:49 -0400

    added cursor sprite and window functionality

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ game_title = "temp"
 
 class MainGame(arcade.Window):
     # constructors for both the derived and the parent classes
-    def __init__(self, width, height, title)
+    def __init__(self, width, height, title):
         super().__init__(width, height, title)
 
     # declaring the instance variables ---> maybe these are considered class variables?
@@ -20,18 +20,21 @@ class MainGame(arcade.Window):
 
     """all variable/sprites will be setup in the setup function"""
     def setup(self):
-        pass
+        self.mouse_cursor_sprite = arcade.Sprite("game_assets/Cursor1.png", center_x=300, center_y=300)
+
 
     """all drawing of sprites and the game screen will be done in the on_draw function"""
     def on_draw(self):
-        pass
+        arcade.start_render()
+        arcade.set_background_color(arcade.color.RED)
+        self.mouse_cursor_sprite.draw()
 
     """game logic will be in the update function"""
     def update(self, delta_time):
         pass
 
     """mouse cursor sprite logic will be in the on_mouse_motion function"""
-    def on_mouse_motion(self, x: float, y: float, dx: float, dy: float, button):
+    def on_mouse_motion(self, x: float, y: float, dx: float, dy: float):
         pass
 
     """all logic surrounding button presses will be in the on_mouse_press function"""
@@ -45,6 +48,9 @@ class MainGame(arcade.Window):
 """main function"""
 def main():
     game = MainGame(screen_width, screen_height, game_title)
+    game.setup()
+    game.on_draw()
+    arcade.run()
 
 if __name__ == "__main__":
     main()
