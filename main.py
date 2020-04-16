fefb125ad86e1ae5e019527b56ab50bdd2505d58
David Rochon <drochon59@yahoo.com>
2020-04-15 20:35:57 -0400

    attempt to make the menu sound play, needs fixing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import arcade
+import time
+# TODO: fix sound, find a way to keep it from playing over itself
 
 # global variables
 screen_width = 600
@@ -22,13 +24,22 @@ class MainGame(arcade.Window):
 
     mouse_cursor_sprite = None
 
-    """all variable/sprites will be setup in the setup function"""
+    current_sound = None
+    menu_sound = None
 
+    """all variable/sprites will be setup in the setup function"""
     def setup(self):
         self.mouse_cursor_sprite = arcade.Sprite("game_assets/Cursor1.png", center_x=300, center_y=300)
 
         self.menu_background = arcade.load_texture("game_backgrounds/menu_background_woods.jpg", width=600, height=600)
         self.current_background = self.menu_background
+
+        self.menu_sound = arcade.load_sound("game_sounds/menu_sound_woods_ambiance.mp3")
+        self.current_sound = self.menu_sound
+
+    def play_song(self, song):
+        arcade.play_sound(song)
+        time.sleep(song.get_length())
 
     """all drawing of sprites and the game screen will be done in the on_draw function"""
     def on_draw(self):
@@ -37,8 +48,12 @@ class MainGame(arcade.Window):
                                       self.current_background)
         self.mouse_cursor_sprite.draw()
 
-    """game logic will be in the update function"""
+        # arcade.play_sound(self.current_sound)
+        # arcade.pause(self.current_sound.get_length())
+        # time.sleep(self.current_sound.get_length())
+        self.play_song(self.current_sound)
 
+    """game logic will be in the update function"""
     def update(self, delta_time):
         pass
 
@@ -65,7 +80,6 @@ class MainGame(arcade.Window):
 def main():
     game = MainGame(screen_width, screen_height, game_title)
     game.setup()
-    game.on_draw()
     arcade.run()
 
 
